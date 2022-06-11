d705374a1a02db51436f2e46ff4036b05d0f6507
yedi <105731655+lyedi@users.noreply.github.com>
2022-06-11 15:42:57 -0400

    parte 1 del clone link tree

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script setup>
+
+import item from './components/item.vue'
+
+</script>
+
+
+<script>
+import datos from './datos/services.json'
+
+
+export default {
+  data() {
+    return {
+      servicio: null
+    }
+  },
+
+  mounted() {
+    this.servicio =   datos;
+    console.log(datos)
+  },
+}
+</script>
+
+<template>
+  <img alt="Vue logo" style="width:70px" src="./assets/logo.png" />
+  <h1> Sea bienvenido(a)!</h1>
+<hr/>
+  <div class="items">
+
+      <item
+        v-for="servici, index in  servicio"
+        :key="index"
+        :item="servici"
+      />
+  </div>
+ 
+</template>
+
+
+
+<!-- cd likn-services
+  npm install
+  npm run dev -->
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+html{
+  background: url("https://cdn.pixabay.com/photo/2014/08/01/00/08/pier-407252_960_720.jpg") ;
+  background-repeat: no-repeat ;
+  background-size: cover ;
+  min-height: 100%;
+}
+.items{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
---
+++ src/components/item.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="card">
+      <h1>
+
+        <a :href="item.href"
+        >  
+          <img :src="item.img" alt="Icono de servicio" />
+
+          <span>   {{item.titulo}} </span>
+
+        </a>
+        
+          
+      </h1>
+  </div>
+</template>
+
+<script>
+export default {
+props: ['item']
+}
+</script>
+
+
+
+<style scoped>
+div{
+  margin: 20px 50px 20px 50px;
+  display: block;
+  width: 100px;
+
+}
+img{
+  width: 70px;
+
+}
+span{
+  font-weight: 900;
+}
+ .card{
+   display: flex;
+   justify-content: center;
+   flex-direction: column;
+   min-width: 120px;
+   min-height: 150px;
+   border: 1px solid grey;
+   border-radius: 5%;
+   box-shadow: 0 0 2em grey;
+   opacity: .9;
+   transition: box-shadow .5s;
+
+ }
+
+ .card:hover{
+   opacity: 1;
+
+   box-shadow: 0 0 4em grey;
+ }
+
+</style>>
+
